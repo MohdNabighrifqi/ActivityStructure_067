e21222af76b2e63aeaadeecc129a4d5cadf2c3f3
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-05 10:29:31 +0700

    memasukan entitas atau variabel

--- StructureAct/StructureAct.cpp
+++ StructureAct/StructureAct.cpp
@@ -8,3 +8,14 @@ struct mahasiswa
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140067";
+	mhs1.umur = 19;
+	mhs1.nama = "Mohd Nabigh Rifqi";
+	mhs1.alamat = "TanjungPinang";
+	
+	
+}
