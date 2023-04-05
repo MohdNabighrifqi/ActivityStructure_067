0d0edc788e3c1d8a2a7b73d8046ef77c8d868437
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-05 10:45:33 +0700

    memasukan integer

--- StructureAct/StructureAct.cpp
+++ StructureAct/StructureAct.cpp
@@ -16,23 +16,24 @@ int main() {
 	mhs1.umur = 19;
 	mhs1.nama = "Mohd Nabigh Rifqi";
 	mhs1.alamat = "TanjungPinang";
-	
+
 	cout << "masukan NIM :";
 	cin >> mhs2.NIM;
 	cout << "masukan umur :";
 	cin >> mhs2.umur;
-	cout << "masukan alamat :";
-	cin >> mhs2.alamat;
 	cout << "masukan nama :";
 	cin >> mhs2.nama;
+	cout << "masukan alamat :";
+	cin >> mhs2.alamat;
 
 	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nUmur = " << mhs1.umur;
 	cout << "\nNama = " << mhs1.nama;
 	cout << "\nAlamat = " << mhs1.alamat;
-	cout << "\nUmur = " << mhs1.umur;
+
 
 	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\n\nUmur = " << mhs2.umur;
 	cout << "\n\nNama = " << mhs2.nama;
 	cout << "\n\nAlamat = " << mhs2.alamat;
-	cout << "\n\nUmur = " << mhs2.umur;
 }
